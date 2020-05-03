a987db1426c914e606cd34f45c7de63fe0c8c0c2
Paul Ferguson Maxson <paul.f.maxson@gmail.com>
2020-05-03 18:36:04 -0400

    transitionTime implementation complete. Closes #8

--- src/DecorationHandler.ts
+++ src/DecorationHandler.ts
@@ -1,44 +1,58 @@
-import * as vscode from 'vscode';
-import * as ConfigHandler from './ConfigHandler';
+import * as vscode from "vscode";
+import * as ConfigHandler from "./ConfigHandler";
 
 export default class DecorationHandler {
-    public getDecorationType(): vscode.TextEditorDecorationType {
-        let configHandler = new ConfigHandler.ConfigHandler();
-        let decorationOptions = configHandler.getDecorationOptions();
-        let decorationType: vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({
-            fontWeight: decorationOptions.fontWeight,
-            fontStyle: decorationOptions.fontStyle,
-            letterSpacing: decorationOptions.letterSpacing,
-            outline: decorationOptions.outline,
-            border: decorationOptions.border,
-            textDecoration: decorationOptions.textDecoration,
-            backgroundColor: decorationOptions.backgroundColor,
-            overviewRulerLane: vscode.OverviewRulerLane.Left,
-            overviewRulerColor: decorationOptions.backgroundColor,
-        });
-        return decorationType;
-    }
+  public getDecorationType(): vscode.TextEditorDecorationType {
+    let configHandler = new ConfigHandler.ConfigHandler();
+    let decorationOptions = configHandler.getDecorationOptions();
+    let decorationType: vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType(
+      {
+        fontWeight: decorationOptions.fontWeight,
+        fontStyle: decorationOptions.fontStyle,
+        letterSpacing: decorationOptions.letterSpacing,
+        outline: decorationOptions.outline,
+        border: decorationOptions.border,
+        textDecoration: decorationOptions.textDecoration,
+        backgroundColor: decorationOptions.backgroundColor,
+        overviewRulerLane: vscode.OverviewRulerLane.Left,
+        overviewRulerColor: decorationOptions.backgroundColor,
+        transitionTime: decorationOptions.transitionTime,
+      }
+    );
+    return decorationType;
+  }
 }
 
 export class DecorationOptions {
-    public readonly fontWeight?: string;
-    public readonly fontStyle?: string;
-    public readonly letterSpacing?: string;
-    public readonly outline?: string;
-    public readonly border?: string;
-    public readonly backgroundColor?: string;
-    public readonly textDecoration?: string;
-    public readonly overviewColor?: string;
+  public readonly fontWeight?: string;
+  public readonly fontStyle?: string;
+  public readonly letterSpacing?: string;
+  public readonly outline?: string;
+  public readonly border?: string;
+  public readonly backgroundColor?: string;
+  public readonly textDecoration?: string;
+  public readonly overviewColor?: string;
+  public readonly transitionTime?: string;
 
-    constructor(fontWeight?: string, fontStyle?: string, letterSpacing?: string, outline?: string, border?: string, textDecoration?: string, backgroundColor?: string) {
-        this.fontWeight = fontWeight;
-        this.fontStyle = fontStyle;
-        this.letterSpacing = letterSpacing;
-        this.outline = outline;
-        this.border = border;
-        this.textDecoration = textDecoration;
-        this.backgroundColor = backgroundColor;
-    }
+  constructor(
+    fontWeight?: string,
+    fontStyle?: string,
+    letterSpacing?: string,
+    outline?: string,
+    border?: string,
+    textDecoration?: string,
+    backgroundColor?: string,
+    transitionTime?: string
+  ) {
+    this.fontWeight = fontWeight;
+    this.fontStyle = fontStyle;
+    this.letterSpacing = letterSpacing;
+    this.outline = outline;
+    this.border = border;
+    this.textDecoration = textDecoration;
+    this.backgroundColor = backgroundColor;
+    this.transitionTime = transitionTime;
+  }
 }
 
 export { DecorationHandler };
--- src/GlobalsHandler.ts
+++ src/GlobalsHandler.ts
@@ -1,82 +1,88 @@
-import * as vscode from 'vscode';
-import DecorationHandler, { DecorationOptions } from './DecorationHandler';
-import * as ConfigHandler from './ConfigHandler';
+import * as vscode from "vscode";
+import DecorationHandler, { DecorationOptions } from "./DecorationHandler";
+import * as ConfigHandler from "./ConfigHandler";
 
 enum SearchDirection {
-    FORWARDS,
-    BACKWARDS
+  FORWARDS,
+  BACKWARDS,
 }
 
 export default class GlobalsHandler {
+  public configHandler: ConfigHandler.ConfigHandler;
 
-    public configHandler: ConfigHandler.ConfigHandler;
+  public decorationStatus: boolean;
+  public decorationTypes: Array<vscode.TextEditorDecorationType>;
+  public searchDirection: SearchDirection;
+  public handleTextSelectionEventActive: boolean;
+  public disableTimer: any;
 
-    public decorationStatus: boolean;
-    public decorationTypes: Array<vscode.TextEditorDecorationType>;
-    public searchDirection: SearchDirection;
-    public handleTextSelectionEventActive: boolean;
-    public disableTimer: any;
+  /* Config parameters */
+  public blurOutOfScopeText!: boolean;
+  public opactiy!: string; // NOTE: should this be "opacity"?
+  public activeWhenDebugging!: boolean;
+  public maxLineSearchCount!: number;
+  public decorationOptions!: DecorationOptions;
+  public enabledLanguages!: Array<string>;
+  public reverseSearchEnabled!: boolean;
+  public allowedStartSymbols!: Array<string>;
+  public allowedEndSymbols!: Array<string>;
+  public highlightScopeFromText!: boolean;
+  public extensionEnabled!: boolean;
+  public lastSelection: vscode.Selection | undefined;
+  public timeOutValue!: number;
+  public ignoreContent!: boolean;
+  public transitionTime!: number;
 
+  constructor() {
+    this.configHandler = new ConfigHandler.ConfigHandler();
+    this.decorationStatus = false;
+    this.decorationTypes = [];
+    this.searchDirection = SearchDirection.FORWARDS;
+    this.handleTextSelectionEventActive = true;
+    this.disableTimer = <any>null;
+    this.transitionTime = 0;
+
+    this.onConfigChange();
+  }
+
+  public onConfigChange() {
     /* Config parameters */
-    public blurOutOfScopeText!: boolean;
-    public opactiy!: string;
-    public activeWhenDebugging!: boolean;
-    public maxLineSearchCount!: number;
-    public decorationOptions!: DecorationOptions;
-    public enabledLanguages!: Array<string>;
-    public reverseSearchEnabled!: boolean;
-    public allowedStartSymbols!: Array<string>;
-    public allowedEndSymbols!: Array<string>;
-    public highlightScopeFromText!: boolean;
-    public extensionEnabled!: boolean;
-    public lastSelection: vscode.Selection | undefined;
-    public timeOutValue!: number;
-    public ignoreContent!: boolean;
+    this.blurOutOfScopeText = this.configHandler.blurOutOfScopeText();
+    this.opactiy = this.configHandler.getOpacity();
+    this.activeWhenDebugging = this.configHandler.activeWhenDebugging();
+    this.maxLineSearchCount = this.configHandler.getMaxLineSearchCount();
+    this.decorationOptions = this.configHandler.getDecorationOptions();
+    this.enabledLanguages = this.configHandler.getEnabledLanguages();
+    this.reverseSearchEnabled = this.configHandler.reverseSearchEnabled();
+    this.allowedStartSymbols = this.configHandler.getAllowedStartSymbols();
+    this.allowedEndSymbols = this.configHandler.getAllowedEndSymbols();
+    this.highlightScopeFromText = this.configHandler.highlightScopeFromText();
+    this.extensionEnabled = this.configHandler.isExtensionEnabled();
+    this.timeOutValue = this.configHandler.getTimeOutValue();
+    this.ignoreContent = this.configHandler.ignoreContent();
+    this.transitionTime = this.configHandler.getTransitionTimeValue();
+  }
 
+  public getLongestSymbolLength(): number {
+    let longestStartSymbolLength = this.getLongestStartSymbolLength();
+    let longestEndSymbolLength = this.getLongestEndSymbolLength();
+    return longestStartSymbolLength > longestEndSymbolLength
+      ? longestStartSymbolLength
+      : longestEndSymbolLength;
+  }
 
-    constructor() {
-        this.configHandler = new ConfigHandler.ConfigHandler();
-        this.decorationStatus = false;
-        this.decorationTypes = [];
-        this.searchDirection = SearchDirection.FORWARDS;
-        this.handleTextSelectionEventActive = true;
-        this.disableTimer = <any>null;
-
-        this.onConfigChange();
-    }
-
-    public onConfigChange() {
-        /* Config parameters */
-        this.blurOutOfScopeText = this.configHandler.blurOutOfScopeText();
-        this.opactiy = this.configHandler.getOpacity();
-        this.activeWhenDebugging = this.configHandler.activeWhenDebugging();
-        this.maxLineSearchCount = this.configHandler.getMaxLineSearchCount();
-        this.decorationOptions = this.configHandler.getDecorationOptions();
-        this.enabledLanguages = this.configHandler.getEnabledLanguages();
-        this.reverseSearchEnabled = this.configHandler.reverseSearchEnabled();
-        this.allowedStartSymbols = this.configHandler.getAllowedStartSymbols();
-        this.allowedEndSymbols = this.configHandler.getAllowedEndSymbols();
-        this.highlightScopeFromText = this.configHandler.highlightScopeFromText();
-        this.extensionEnabled = this.configHandler.isExtensionEnabled();
-        this.timeOutValue = this.configHandler.getTimeOutValue();
-        this.ignoreContent = this.configHandler.ignoreContent();
-
-    }
-
-    public getLongestSymbolLength(): number {
-        let longestStartSymbolLength = this.getLongestStartSymbolLength();
-        let longestEndSymbolLength = this.getLongestEndSymbolLength();
-        return longestStartSymbolLength > longestEndSymbolLength ? longestStartSymbolLength : longestEndSymbolLength;
-    }
-
-    public getLongestStartSymbolLength(): number {
-        let startSymbols = this.allowedStartSymbols;
-        return startSymbols.reduce(function (a, b) { return a.length > b.length ? a : b; }).length;
-    }
-    public getLongestEndSymbolLength(): number {
-        let endSymbols = this.allowedEndSymbols;
-        return endSymbols.reduce(function (a, b) { return a.length > b.length ? a : b; }).length;
-    }
+  public getLongestStartSymbolLength(): number {
+    let startSymbols = this.allowedStartSymbols;
+    return startSymbols.reduce(function (a, b) {
+      return a.length > b.length ? a : b;
+    }).length;
+  }
+  public getLongestEndSymbolLength(): number {
+    let endSymbols = this.allowedEndSymbols;
+    return endSymbols.reduce(function (a, b) {
+      return a.length > b.length ? a : b;
+    }).length;
+  }
 }
 
 var bracketHighlightGlobals: GlobalsHandler = new GlobalsHandler();
